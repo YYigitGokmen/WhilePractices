bf004e32b8ecc11069347d21e2a904ef6ba58eb6
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-07-18 16:50:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+int i = 0;
+while (i <= 9)
+{
+    i++;
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+}
+
+
+Console.WriteLine("---------------------------------");
+
+
+int x = 0;
+while (x <= 19)
+{
+    x++;
+    Console.WriteLine($"{x}");
+}
+
+Console.WriteLine("---------------------------------");
+
+
+int y = 0;
+while ( y <= 18)
+{
+    y += 2;
+    Console.WriteLine($"{y}");
+}
+
+Console.WriteLine("---------------------------------");
+
+
+int sum = 0;
+int z = 49;
+while  (z <= 149)
+{
+    z++;
+    sum += z;
+
+}
+Console.WriteLine($"{sum}");
+//50 ve 150 yi dahil etmek için döngüyü 49 dan başlatıp 149 da bitirdim
+
+
+Console.WriteLine("---------------------------------");
+
+
+
+int odd = 0;
+int even = 0;
+int loop = 0;
+//121 i dahil etmemesi için 119 da kestim döngüyü ve en son çıktı 120 oldu.
+while (loop <= 119)
+{
+    loop++;
+    
+    if (loop % 2 == 0)
+    {
+        even += loop;
+    }
+    else
+    {
+        odd += loop;
+    }
+    
+}
+
+
+Console.WriteLine($"1 ile 120 Arasındaki çift sayıların toplamı {even}");
+
+Console.WriteLine($"1 ile 120 Arasındaki Tek sayıların toplamı {odd}");
+
+
+
